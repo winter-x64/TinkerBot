938326932c60b6ef5a28920d79ea196360870c58
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-11-03 20:03:32 +0530

    ✌️ Formatted file
    Just a simple code formatting using black

--- src/creds.py
+++ src/creds.py
@@ -5,23 +5,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class FirebaseCreds(NamedTuple):
-    type: str = os.environ.get('FIREBASE_TYPE')
-    project_id: str = os.environ.get('FIREBASE_PROJECT_ID')
-    private_key_id: str = os.environ.get('FIREBASE_PRIVATE_KEY_ID')
-    private_key: str = os.environ.get('FIREBASE_PRIVATE_KEY')
-    client_email: str = os.environ.get('FIREBASE_CLIENT_EMAIL')
-    client_id: str = os.environ.get('FIREBASE_CLIENT_ID')
-    auth_uri: str = os.environ.get('FIREBASE_AUTH_URI')
-    token_uri: str = os.environ.get('FIREBASE_TOKEN_URI')
-    auth_provider_x509_cert_url: str = os.environ.get('FIREBASE_AUTH_PROVIDER_X509_CERT_URL')
-    client_x509_cert_url: str = os.environ.get('FIREBASE_CLIENT_X509_CERT_URL')
-    universe_domain: str = os.environ.get('FIREBASE_UNIVERSE_DOMAIN')
+    type: str = os.environ.get("FIREBASE_TYPE")
+    project_id: str = os.environ.get("FIREBASE_PROJECT_ID")
+    private_key_id: str = os.environ.get("FIREBASE_PRIVATE_KEY_ID")
+    private_key: str = os.environ.get("FIREBASE_PRIVATE_KEY")
+    client_email: str = os.environ.get("FIREBASE_CLIENT_EMAIL")
+    client_id: str = os.environ.get("FIREBASE_CLIENT_ID")
+    auth_uri: str = os.environ.get("FIREBASE_AUTH_URI")
+    token_uri: str = os.environ.get("FIREBASE_TOKEN_URI")
+    auth_provider_x509_cert_url: str = os.environ.get(
+        "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
+    )
+    client_x509_cert_url: str = os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+    universe_domain: str = os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
 
     def to_json(self) -> Dict[str, str]:
-        var = [i for i in vars(FirebaseCreds).keys() if not i.startswith('_') and i != 'to_json']
+        var = [
+            i
+            for i in vars(FirebaseCreds).keys()
+            if not i.startswith("_") and i != "to_json"
+        ]
         return {i: getattr(self, i) for i in var}
 
 
 class BotCreds(NamedTuple):
-    bot_token: str = os.environ.get('BOT_TOKEN')
+    bot_token: str = os.environ.get("BOT_TOKEN")
